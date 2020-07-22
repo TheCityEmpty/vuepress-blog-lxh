251b81e3aee60b3de21938d1acd4bf39757dcff4
TheCityEmpty <lxhaweb@163.com>
2020-07-22 20:32:06 +0800

    test deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,30 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
-
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# 初始化git 仓库
 git init
-git add -A
-git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
-# git push -f git@github.com:TheCityEmpty/TheCityEmpty.github.io.git master vuepress-blog-lxh
+# 将打包文件直接添加进缓冲区
+git add .
+git commit -m "update"
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
-git push -f git@github.com:TheCityEmpty/vuepress-blog-lxh.git master
+echo '以下是本地分支：'
+git branch -a
+
+echo '更新远程分支....'
+git fetch
+
+echo '更新完成的分支：'
+git branch -a
+
+echo '将本地master 分支关联远程master分支'
+git branch --set-upstream-to=origin/master master
+
+echo '强制push, 替换掉打包文件'
+git push -f
+
 
 cd -
